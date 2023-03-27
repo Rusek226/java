1967ef961d52bc84ff98b3d5846c0132cc7d4fb0
Marcin Rusiniak <marcin.rusiniak@gamil.com>
2023-03-27 23:36:10 +0200

    Kalkulator, Inkrementacja, Dekrementacja,Scanner imię

--- src/InkrementacjaDekrementacja.java
+++ src/InkrementacjaDekrementacja.java
@@ -3,12 +3,28 @@ public class InkrementacjaDekrementacja {
     public static void main(String[] args) {
         int a = 0;
 
-        System.out.println("Wartość a: " + a);
+        System.out.println("Wartość a: " + a);//0
         int b = a++;
-        System.out.println("Wartość b: " + b);
-        System.out.println("Wartość a: " + a);
+        System.out.println("Wartość b: " + b);//0
+        System.out.println("Wartość a: " + a);//1
         int c = ++a;
-        System.out.println("Wartość c: " + c);
-        System.out.println("Wartość a: " + a);
+        System.out.println("Wartość c: " + c);//2
+        System.out.println("Wartość a: " + a);//2
+
+        // Dekrementacja zmniejszenie wartości o jeden
+
+        int d =0;
+        System.out.println("Wartość d: " +d);//0
+        int e = d--;
+        System.out.println("Wartość e: " +e );//0
+        System.out.println("Wartośc d: " +d);//-1
+
+        int f = --d;
+        System.out.println("Wartośc f: " +d);//-2
+        System.out.println("Wartosć d: " +f);//-2
     }
 }
+
+
+
+
