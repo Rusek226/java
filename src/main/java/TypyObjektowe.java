57f18a5844960bc7484582bc4d618fcc0967ae55
Marcin Rusiniak <marcin.rusiniak@gamil.com>
2023-03-28 01:24:30 +0200

    Test2

---
+++ src/main/java/TypyObjektowe.java
@@ -0,0 +1,33 @@
+public class TypyObjektowe {
+
+
+    public static void main(String[] args) {
+
+
+
+        //liczby stałoprzecinkowe
+        byte firstNumber = 127;// 1 bajt
+        short secondNumber = 32689;// 2 bajty
+        int thirdNumber = 3276899;// 4 bajty
+        long fourthNumber = 2148000L;// 8 bajtów
+
+// liczby zmiennoprzecinkowe
+
+        float fifthNumber = 4.99934F;// 4 bajty max 4-6 liczb po przecinku
+        double sixNumber = 3.999999999999999D; // 8 bajtów max 15 liczb po przecinku
+
+        // Wartość logiczna
+        boolean prawda = true;
+        boolean falsz = false;
+
+//  Pojedyńczy znak
+
+        char letter = 'A';
+
+        String hello = "Hello Marcin";
+
+
+
+    }
+
+}
---
+++ src/main/java/Zmienne.java
@@ -0,0 +1,11 @@
+// Zarezerwowane miejsce z określonym typem danych.
+public class Zmienne {
+
+   String name = "Marcin";
+
+    public static void main(String[] args) {
+        String name = "Marcin";
+        System.out.println(name);
+    }
+
+}
